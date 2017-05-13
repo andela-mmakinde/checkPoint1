ddee9c180d31d993d934889897fa726957f9d645
Andela Developer <andeladeveloper@Mayor.local>
2017-05-13 23:30:49 +0100

    fix bug to host on heroku

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ const app = express();
 
 const router = express.Router();
 
-const filePath = path.join(__dirname, 'public/');
+const fpath = path.join(__dirname, 'public/');
 
-app.use(express.static(filePath));
+app.use(express.static(fpath));
 
-router.get('/', (req, res) => {
-  res.sendFile(`${filePath}index.html`);
-});
+router.get("/",function(req,res){
+    res.sendFile(fpath + "index.html")
+})
 
 
-router.get('*', (req, res) => {
-  res.send('404 not found');
-});
+router.get("*",function(req,res){
+    res.send("404 not found")
+})
 
-app.use('/', router);
+app.use("/",router)
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
