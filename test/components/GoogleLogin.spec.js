e4f8b58344bcc15507484b04c9631a7ad65c7e5c
Andela Developer <mayowa.makinde@andela.com>
2017-05-31 18:15:21 +0100

    added test for googleLogin component

--- test/components/GoogleLogin.spec.js
+++ test/components/GoogleLogin.spec.js
@@ -1,41 +1,41 @@
-// import { expect } from 'chai';
-// import sinon from 'sinon';
-// import { responseGoogle, logout } from '../../src/js/components/GoogleLogin.jsx';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { responseGoogle, logout } from '../../src/js/components/GoogleLogin.jsx';
 
-// describe('The function responseGoogle()', () => {
-//   it('should store user data in the local storage', () => {
-//     const profile = {
-//       getBasicProfile: () => (
-//         {
-//           getName: () => (
-//             'mayor'
-//           ),
-//           getEmail: () => (
-//             'mail'
-//           ),
-//           getImageUrl: () => (
-//             'image'
-//           ),
-//         }
-//       ),
-//     };
-//     const locationSpy = sinon.spy(location, 'reload');
-//     location.reload();
-//     sinon.assert.calledOnce(locationSpy);
-//     const expectedStorage = { user: '{"username":"mayor","email":"mail","image":"image"}' };
-//     expect(locationSpy.calledOnce).to.be.true;
-//     responseGoogle(profile);
-//     expect(localStorage).to.eqls(expectedStorage);
-//     locationSpy.restore();
-//   });
-//   describe('The function logout()', () => {
-//     it('contains a method preventDefault that initializes when logout() is called', () => {
-//       const evt = {
-//         preventDefault: sinon.spy(),
-//       };
-//       logout(evt);
-//       expect(evt.preventDefault.calledOnce).to.be.true;
-//     });
-//   });
-// });
+describe('The function responseGoogle()', () => {
+  it('should store user data in the local storage', () => {
+    const profile = {
+      getBasicProfile: () => (
+        {
+          getName: () => (
+            'mayor'
+          ),
+          getEmail: () => (
+            'mail'
+          ),
+          getImageUrl: () => (
+            'image'
+          ),
+        }
+      ),
+    };
+    const locationSpy = sinon.spy(location, 'reload');
+    location.reload();
+    sinon.assert.calledOnce(locationSpy);
+    const expectedStorage = { user: '{"username":"mayor","email":"mail","image":"image"}' };
+    expect(locationSpy.calledOnce).to.be.true;
+    responseGoogle(profile);
+    expect(localStorage).to.eqls(expectedStorage);
+    locationSpy.restore();
+  });
+  describe('The function logout()', () => {
+    it('contains a method preventDefault that initializes when logout() is called', () => {
+      const evt = {
+        preventDefault: sinon.spy(),
+      };
+      logout(evt);
+      expect(evt.preventDefault.calledOnce).to.be.true;
+    });
+  });
+});
 
